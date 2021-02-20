1f17a3fe30651e64b75aac8720facbfc46f72df9
Andres Felipe Sossa Sanchez <anfesos@hotmail.com>
2021-02-20 09:49:23 -0500

    Actualización de archivo .gitignore, y userinterface

--- src/main/java/co/com/automatizacion/google/userinterface/PaginaInicial.java
+++ src/main/java/co/com/automatizacion/google/userinterface/PaginaInicial.java
@@ -5,6 +5,6 @@ import net.thucydides.core.pages.PageObject;
 import org.openqa.selenium.By;
 
 public class PaginaInicial extends PageObject {
-    public static Target BUSCADOR_INPUT = Target.the("Cuadro de ingreso de información a buscar").located(By.xpath("//*[@id='tsf']/div[2]/div[1]/div[1]/div/div[2]/input"));
+    public static Target BUSCADOR_INPUT = Target.the("Cuadro de ingreso de información a buscar").located(By.xpath("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input"));
     public static Target RESULTADO_BUSQUEDA = Target.the("Información de registros arrojados por la consulta").located(By.xpath("//*[@id='result-stats']"));
 }
